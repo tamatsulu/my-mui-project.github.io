eea6b5b12db22750b74831da2cb52c21d66d2054
tamatsulu <161196771+tamatsulu@users.noreply.github.com>
2025-03-17 22:15:33 +0900

    Resolve merge conflict in README.md

--- src/App.js
+++ src/App.js
@@ -154,3 +154,30 @@ export default function PersistentDrawerLeft() {
     </Box>
   );
 }
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> ec13fb8 (Resolve merge conflict in README.md)
