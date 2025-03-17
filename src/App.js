fbd3fcb5065e366f4ff79226d913775d6cb0afcd
tamatsulu <161196771+tamatsulu@users.noreply.github.com>
2025-03-17 20:49:59 +0900

    Add basic layout with MUI v5

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-end',
 }));
 
-export default function PersistentDrawerLeft() {
+function PersistentDrawerLeft() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -154,30 +154,5 @@ export default function PersistentDrawerLeft() {
     </Box>
   );
 }
-=======
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> ec13fb8 (Resolve merge conflict in README.md)
+export default PersistentDrawerLeft;
